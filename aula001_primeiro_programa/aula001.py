3b5076af0035234fa1952186116eda8be38b3a87
DianLuca <dianlucca@hotmail.com>
2024-04-12 21:20:14 -0300

    First Hello World

---
+++ aula001_primeiro_programa/aula001.py
@@ -0,0 +1,17 @@
+# Curso de Desenvolvimento de Sistemas
+# Autor: Dian Luca Valente Nascimento
+# Data: 12/04/2024
+
+# Importando biblioteca
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+# O comando print() executa uma saída
+
+print('-' * 70)
+print('Hello World!!!')
+print('-' * 70)
+print()
