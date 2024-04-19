66e31750eb7ac913d586c4fb0feb574d4138d6f3
DianLuca <dianlucca@hotmail.com>
2024-04-19 19:35:32 -0300

    Correção na atividade i.py

--- atividades_ate_aula004/i.py
+++ atividades_ate_aula004/i.py
@@ -21,13 +21,13 @@ valor_real = float(input('Insira um valor em reais para conversão: '))
 # Processamento
 valor_dolar = 5.20 # Cotação de 19/04/2024
 
-conversao_real_dolar = valor_dolar * valor_real
-conversao_dolar_real = valor_real / valor_dolar
+conversao_dolar_real = valor_dolar * valor_real
+conversao_real_dolar = valor_real / valor_dolar
 
 # Saída
 print('-' * 70)
 print('--- CONVERSÃO DE VALORES')
 print('=' * 70)
-print(f'Um dólar corresponde a: R$ {conversao_real_dolar:.2f} e '
-      + f'R${valor_real:.2f} correspondem a: $ {conversao_dolar_real:.2f}' 
+print(f'$ {valor_real:.2f} dólar(es) corresponde a: R$ {conversao_dolar_real:.2f} e '
+      + f'R${valor_real:.2f} correspondem a: $ {conversao_real_dolar:.2f}' 
       + f'(dólares)')
