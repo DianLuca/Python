f9ccde3e4feb287ae93cbbd8ca62fc7446aa4061
DianLuca <dianlucca@hotmail.com>
2024-04-19 13:56:00 -0300

    Execução da atividade f.py

--- atividades_ate_aula004/f.py
+++ atividades_ate_aula004/f.py
@@ -4,3 +4,24 @@
 # Data: 19/04/2024
 # Faça um programa que receba um número qualquer e calcule o dobro e o triplo 
 # desse número.
+
+# Imports
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ENTRADA DE DADOS')
+print('-' * 70)
+
+# Entrada
+valor = float(input('Insira qualquer valor: '))
+
+# Processamento
+dobro = valor * 2
+triplo = valor * 3
+
+# Saída
+print(f'O dobro e o triplo do valor {valor} são respectivamente: '
+      + f'{dobro} e {triplo}')
