38de8b55e825adf7d09ac86eb33adf210f55ed1d
DianLuca <dianlucca@hotmail.com>
2024-04-25 23:23:12 -0300

    Desenvolvimento da atividade002 b.py

--- atividade002/b.py
+++ atividade002/b.py
@@ -6,9 +6,19 @@ valor1 = input('Insira um valor:')
 valor2 = input('Insira um valor:')
 valor3 = input('Insira um valor:')
 
-if valor1 > valor2 and valor1 > valor3:
+# Para maior
+if valor1 > valor2 > valor3:
     print(f'O valor {valor1} é maior que os valores: {valor2} e {valor3}')
-elif valor2 > valor1 and valor2 > valor3:
+elif valor2 > valor1 > valor3:
     print(f'O valor {valor2} é maior que os valores: {valor1} e {valor3}')
 else:
-    print(f'O valor {valor3} é maior que os valores: {valor1} e {valor2}')  
+    print(f'O valor {valor3} é maior que os valores: {valor1} e {valor2}')
+
+
+# Para menor
+if valor1 < valor2 < valor3:
+    print(f'O valor {valor1} é menor que os valores: {valor2} e {valor3}')
+elif valor2 < valor1 < valor3:
+    print(f'O valor {valor2} é menor que os valores: {valor1} e {valor3}')
+else:
+    print(f'O valor {valor3} é menor que os valores: {valor1} e {valor2}')
