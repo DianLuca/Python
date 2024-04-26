c69a26abe947d8230f910b7f2352ad14ea2bb18a
Dian Luca <dianlucca@hotmail.com>
2024-04-26 14:30:51 -0300

    Desenvolvimento da atividade002 f.py

--- atividade002/f.py
+++ atividade002/f.py
@@ -10,3 +10,26 @@
 # Além disso, é necessário realizar a validação de entrada de dados para 
 # garantir que o ano inserido pelo usuário seja um valor válido, ou seja, um 
 # número inteiro positivo.
+
+# Import
+import os
+
+
+os.system('cls')
+
+# Entrada
+ano = int(input('Insira um ano: '))
+resposta = ''
+
+# Condicionais
+if ano < 0:
+    resposta = f'O {ano} inserido é um valor inválido'
+elif ano % 4 == 0:
+    resposta = f'O ano {ano} é um ano bissexto'
+else:
+    resposta = f'O ano {ano} não é um ano bissexto'
+
+# Saída
+print()
+print(resposta)
+print('-' * 70)
