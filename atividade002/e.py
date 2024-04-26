81a9a4ae310844a4bd431a754a95c2e07bf31443
Dian Luca <dianlucca@hotmail.com>
2024-04-26 14:03:53 -0300

    Pequena correção na linha 14

--- atividade002/e.py
+++ atividade002/e.py
@@ -10,8 +10,8 @@ distancia_maior_200 = 0.4  # R$ 0,40
 
 if 0 < distancia_usuario <= 200:
     valor_total = distancia_200 * distancia_usuario
-    resposta = f'O valor da sua viagem de {distancia_usuario} KM será de: ' 
-    + f'R$ {valor_total:.2f}'
+    resposta = (f'O valor da sua viagem de {distancia_usuario} KM será de: ' 
+    + f'R$ {valor_total:.2f}')
 elif distancia_usuario > 200:
     valor_total = distancia_maior_200 * distancia_usuario
     resposta = (f'O valor da sua viagem de {distancia_usuario} KM será de: '
