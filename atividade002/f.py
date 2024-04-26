b723fc3aa779c1a8cebd46f6f4187e5c75edaff1
DianLuca <dianlucca@hotmail.com>
2024-04-25 21:15:00 -0300

    Criação do cabeçalho das atividades f, g e h

---
+++ atividade002/f.py
@@ -0,0 +1,12 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Dian Luca Valente Nascimento
+# Data: 19/04/2024
+# F) A empresa "LeapYearCheck" está desenvolvendo um software de verificação
+# de anos bissextos para auxiliar usuários na identificação desses anos de 
+# forma rápida e precisa. Eles precisam de um programa que permita aos 
+# usuários inserir um ano e, em seguida, determine se esse ano é bissexto ou 
+# não, de acordo com as regras estabelecidas pelo calendário gregoriano. 
+# Além disso, é necessário realizar a validação de entrada de dados para 
+# garantir que o ano inserido pelo usuário seja um valor válido, ou seja, um 
+# número inteiro positivo.
