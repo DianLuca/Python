cbe1b7127b2a8aee6829a0df7e020766f7f4e104
Dian Luca <dianlucca@hotmail.com>
2024-04-27 11:01:56 -0300

    Desenvolvimento da atividade002 h.py

--- atividade002/h.py
+++ atividade002/h.py
@@ -3,12 +3,12 @@
 # Autor: Dian Luca Valente Nascimento
 # Data: 26/04/2024
 # H) A empresa "RootCalc" está desenvolvendo um software de cálculo de raízes
-# de equações quadráticas para auxiliar engenheiros e cientistas em suas 
-# análises e projetos. Eles precisam de um programa que calcule as raízes da 
-# equação quadrática 𝑥²−6𝑥+5 sem depender de funções ou métodos predefinidos, 
-# utilizando apenas os conceitos e operações básicas aprendidos até o momento. 
-# Essas raízes são conhecidas como 𝑥’ = 5 e 𝑥’’ = 1, e o programa deve ser 
-# capaz de calcular essas raízes de forma precisa, seguindo os princípios 
+# de equações quadráticas para auxiliar engenheiros e cientistas em suas
+# análises e projetos. Eles precisam de um programa que calcule as raízes da
+# equação quadrática 𝑥²−6𝑥+5 sem depender de funções ou métodos predefinidos,
+# utilizando apenas os conceitos e operações básicas aprendidos até o momento.
+# Essas raízes são conhecidas como 𝑥’ = 5 e 𝑥’’ = 1, e o programa deve ser
+# capaz de calcular essas raízes de forma precisa, seguindo os princípios
 # matemáticos fundamentais.
 
 # Import
@@ -18,16 +18,25 @@ import os
 os.system('cls')
 
 # Declarações
-# 𝑥²−6𝑥+5 onde a = 1, b = -6 c =6
-raiz_quadrada = 1/2
-x1 = 5
-x2 = 1
+# 𝑥²−6𝑥+5 onde
+a = 1
+b = -6
+c = 5
+resposta = ''
 
 # Processamento
-calculo_5 = (x1**2 - 6*x1 + 5) ** raiz_quadrada
-calculo_1 = (x2**2 - 6*x2 + 5) ** raiz_quadrada
-# outro_calculo = (x1**2 - 6*x2 + 5) ** raiz_quadrada
+delta = b**2 - 4*a*c
+raiz_delta = delta**(1/2)
+
+# Condicionais
+if delta < 0:  # Valores negativos não possuem raízes reais
+    resposta = f'Não possuí uma raiz real'
+else:
+    x1 = (-b + raiz_delta)/(2*a)
+    x2 = (-b - raiz_delta)/(2*a)
+    resposta = f'O valor das raízes será igual a: {x1} e {x2}'
 
 # Saída
-print(f'{calculo_5:.2f} e {calculo_1:.2f}')
-# print(outro_calculo)
+print()
+print(resposta)
+print('-' * 70)
