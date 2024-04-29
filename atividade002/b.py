932f68cf7c4723d8c97a9415af00e817c86ca7f6
DianLuca <dianlucca@hotmail.com>
2024-04-29 19:46:28 -0300

    Correção do exercício b.py

--- atividade002/b.py
+++ atividade002/b.py
@@ -2,18 +2,18 @@ import os
 
 os.system('cls')
 
-valor1 = input('Insira um valor: ')
-valor2 = input('Insira um valor: ')
-valor3 = input('Insira um valor: ')
+valor1 = int(input('Insira um valor: '))
+valor2 = int(input('Insira um valor: '))
+valor3 = int(input('Insira um valor: '))
 maior = float()
 menor = float()
 
 # Para maior
-if valor1 > valor2 and valor1 > valor3:
+if (valor1 > valor2 and valor1 > valor3):
     maior = valor1
-elif valor2 > valor1 or valor2 > valor3:
+elif (valor2 > valor1 and valor2 > valor3):
     maior = valor2
-elif valor3 > valor1 or valor3 > valor2:
+elif (valor3 > valor1 and valor3 > valor2):
     maior = valor3
 else:
     print(f'Os valores são iguais')
@@ -22,9 +22,9 @@ else:
 # Para menor
 if valor1 < valor2 and valor1 < valor3:
     menor = valor1
-elif valor2 < valor1 or valor2 < valor3:
+elif valor2 < valor1 and valor2 < valor3:
     menor = valor2
-elif valor3 < valor1 or valor3 < valor2:
+elif valor3 < valor1 and valor3 < valor2:
     menor = valor3
 else:
     print(f'Os valores são iguais')
