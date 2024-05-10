e0c49e14366df5cde3e6d72685db5946653aa326
DianLuca <dianlucca@hotmail.com>
2024-05-10 15:04:09 -0300

    Resolução da atividade f.py

--- atividade003/f.py
+++ atividade003/f.py
@@ -4,3 +4,28 @@
 # Data: 10/05/2024
 # F) Faça um programa onde o usuário tenta adivinhar o número que o computador
 # está ‘pensando’.
+
+import random 
+import os
+
+
+os.system('cls')
+
+print('ADIVINHAÇÃO')
+print('-' * 70)
+
+numero = int(input('Insira um número de 1 a 10 e tente advinhar: '))
+print()
+
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numero_sorteado = random.choice(lista_numeros)
+
+if numero_sorteado == numero:
+    print(f'Você acertou! O número sorteado foi: {numero} = {numero_sorteado}.')
+else:
+    print(f'Não foi dessa vez, o número correto era {numero_sorteado}.'
+          + f'Tente novamente!')
+    
+print()
+print('-' * 70)
