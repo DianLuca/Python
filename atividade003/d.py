79f2b3be5e90321931958ffbb6aa3b7f520261f5
DianLuca <dianlucca@hotmail.com>
2024-05-10 13:32:03 -0300

    Resolução da atividade d.py

--- atividade003/d.py
+++ atividade003/d.py
@@ -4,3 +4,21 @@
 # Data: 10/05/2024
 # D) Faça um programa que receba um ângulo qualquer. Em seguida, calcule o 
 # seno, cosseno e tangente do ângulo, limitando a saída a 2 casas decimais.
+
+import math
+import os
+
+
+os.system('cls')
+
+print('CÁLCULO DE COSSENO, SENO E TANGENTE')
+print('-'* 70)
+
+angulo = float(input('Insira o valor de um ângulo: '))
+
+cos = math.cos(math.radians(angulo))
+seno = math.sin(math.radians(angulo))
+tan = math.tan(math.radians(angulo))
+
+print(f'O angulo de {angulo} possuí: cosseno de: {cos:.2f}, seno de: {seno:.2f}, '
+      + f'tangente de: {tan:.2f}')
