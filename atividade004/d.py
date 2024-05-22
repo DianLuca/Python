a1ab96541fba08f917c3916df9ef2a8784f5a96c
Dian Luca <dianlucca@hotmail.com>
2024-05-22 13:22:35 -0300

    Padronização d.py

--- atividade004/d.py
+++ atividade004/d.py
@@ -1,5 +1,5 @@
 # Faça um programa que leia uma frase e depois exiba na tela:
-# • A frase em minúsculas, a frase em maiúsculas, a quantidade 
+# • A frase em minúsculas, a frase em maiúsculas, a quantidade
 # de caracteres na frase e quantas letras tem a 2ª palavra na frase.
 
 import os
@@ -15,10 +15,11 @@ minuscula = frase.lower()
 maiuscula = frase.upper()
 contagem = len(frase)
 
-print(f'A frase toda minúscula será: "{minuscula}", toda maiúscula: ' 
+print(f'A frase toda minúscula será: "{minuscula}", toda maiúscula: '
       + f'"{maiuscula}" e possui {contagem} caracteres.')
 
 lista = frase.split(' ')
 palavra = ''.join(lista[1:2])
 contagem_segunda_palavra = len(palavra)
-print(f'A segunda palavra da frase é: {lista[1:2]} e possui: {contagem_segunda_palavra}  caracteres.')
+print(f'A segunda palavra da frase é: {lista[1:2]} e possui: '
+      + f'{contagem_segunda_palavra}  caracteres.')
