ed018241fc1d1360d11e1be3789da7ca3747c779
Dian Luca <dianlucca@hotmail.com>
2024-05-22 13:20:53 -0300

    Desenvolvimento da atividade d.py

---
+++ atividade004/d.py
@@ -0,0 +1,24 @@
+# Faça um programa que leia uma frase e depois exiba na tela:
+# • A frase em minúsculas, a frase em maiúsculas, a quantidade 
+# de caracteres na frase e quantas letras tem a 2ª palavra na frase.
+
+import os
+
+
+os.system('cls')
+
+frase = input('Insira uma frase qualquer: ')
+
+print(frase)
+
+minuscula = frase.lower()
+maiuscula = frase.upper()
+contagem = len(frase)
+
+print(f'A frase toda minúscula será: "{minuscula}", toda maiúscula: ' 
+      + f'"{maiuscula}" e possui {contagem} caracteres.')
+
+lista = frase.split(' ')
+palavra = ''.join(lista[1:2])
+contagem_segunda_palavra = len(palavra)
+print(f'A segunda palavra da frase é: {lista[1:2]} e possui: {contagem_segunda_palavra}  caracteres.')
