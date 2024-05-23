f118a6ebf0bbe91d8116fc79c29867e125eeb41b
DianLuca <dianlucca@hotmail.com>
2024-05-23 20:30:13 -0300

    Pequena alteração nos enunciados

--- atividade004/h.py
+++ atividade004/h.py
@@ -2,7 +2,7 @@
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
 # Data: 23/05/2024
-# G) Faça um programa que leia o nome de um aluno e mostre quantas vezes a 
+# H) Faça um programa que leia o nome de um aluno e mostre quantas vezes a 
 # letra 'o' aparece e em que posição ela aparece pela primeira e última vez.
 
 import os
--- atividade004/i.py
+++ atividade004/i.py
@@ -2,7 +2,7 @@
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
 # Data: 23/05/2024
-# G) Faça um programa que receba o nome completo de uma pessoa e, 
+# I) Faça um programa que receba o nome completo de uma pessoa e, 
 # em seguida, mostre o primeiro e o último nome.
 
 import os
