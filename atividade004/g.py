154ae95ced2c823749dba2e012c104a0ee4ee87a
DianLuca <dianlucca@hotmail.com>
2024-05-23 18:53:21 -0300

    Pequena alteração na linha 21 do g.py

--- atividade004/g.py
+++ atividade004/g.py
@@ -18,5 +18,5 @@ dezenas = (numero // 10) % 10
 centenas = (numero // 100) % 10
 milhares = (numero // 1000) % 10
 
-print(f'O número: "{numero}" possui {unidades} unidade(s), {dezenas} '
+print(f'O número: "{numero}" possui: {unidades} unidade(s), {dezenas} '
       + f'dezena(s), {centenas} centena(s) e {milhares} milhar(es)')
