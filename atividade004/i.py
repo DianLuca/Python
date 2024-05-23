49811faa6586cd7e6ac871aea665befc51973cb0
Dian Luca <dianlucca@hotmail.com>
2024-05-23 14:15:53 -0300

    Desenvolvimento da atividade i.py

--- atividade004/i.py
+++ atividade004/i.py
@@ -4,3 +4,16 @@
 # Data: 23/05/2024
 # G) Faça um programa que receba o nome completo de uma pessoa e, 
 # em seguida, mostre o primeiro e o último nome.
+
+import os
+
+
+os.system('cls')
+
+nome_completo = input('Insira seu nome completo: ')
+
+lista = nome_completo.split()
+primeiro_nome = lista[0]
+ultimo_nome = lista[-1]
+
+print(f'Primeiro Nome: {primeiro_nome}\n Último Nome: {ultimo_nome}')
