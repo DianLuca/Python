43b75a77a195e2eb8ad0187319639a53decfc95c
DianLuca <dianlucca@hotmail.com>
2024-05-27 14:32:39 -0300

    Adiçaõ de validação em a.py

--- atividade004/a.py
+++ atividade004/a.py
@@ -9,7 +9,16 @@ os.system('cls')
 nome = input('Insira seu nome: ')
 nome_meio = input('Insira o nome do meio: ')
 sobrenome = input('Insira seu sobrenome: ')
+resposta = ''
 
-lista = [nome, nome_meio, sobrenome]
-juncao = ' '.join(lista)
-print(f'O seu nome completo é: {juncao}')
+# Validação
+if (nome and nome_meio and sobrenome) == str:
+    lista = [nome, nome_meio, sobrenome]
+    juncao = ' '.join(lista)
+    resposta = (f'O seu nome completo é: {juncao}')
+else:
+    resposta = (f'Um dos valores inseridos é inválido, por favor insira apenas ' 
+          + f'valores do tipo texto.')
+    
+print()
+print(resposta)
