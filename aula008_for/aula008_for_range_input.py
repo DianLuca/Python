a77985ef639e394cf17d77d06766488a5176042e
DianLuca <dianlucca@hotmail.com>
2024-05-27 21:23:05 -0300

    Criação da aula008_for_range_input.py

---
+++ aula008_for/aula008_for_range_input.py
@@ -0,0 +1,16 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE FOR RANGE')
+print('=' * 70)
+
+print()
+
+for var_iteradora in range(1,5): # Para cada variável iteradora no valor de 1 a 5
+    cor = str(input(f'Digite a {var_iteradora}ª cor: '))
+    
+print()
+print()
