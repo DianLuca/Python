f0bdb46576a1024690cd6d48eab33a7ad55c9bd9
DianLuca <dianlucca@hotmail.com>
2024-05-29 22:06:25 -0300

    Desenvolvimento da atividade a.py

--- atividade005/a.py
+++ atividade005/a.py
@@ -1,2 +1,14 @@
 # Faça um programa que imprima os números no intervalo entre 1 e 100. 
 # Os números devem ser apresentados na horizontal.
+
+import os 
+
+
+os.system('cls')
+
+
+contador = 0
+
+for c in range(1, 101):
+    print(c, end = ' - ')
+    c += 1
