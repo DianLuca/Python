9596427f6f6f31074db9966c701aeab649a3c3be
DianLuca <dianlucca@hotmail.com>
2024-05-31 14:54:41 -0300

    Alterações na f.py

--- atividade005/f.py
+++ atividade005/f.py
@@ -7,11 +7,11 @@ os.system('cls')
 print('NÚMEROS PRIMOS')
 print()
 
-a = int(input('Insira um número para começar: ')) # Inserir números maiores que 2
+a = int(input('Insira um número para começar: ')) # vide linha 14
 b = int(input('Insira um número para finalizar:'))
 
 for c in range(a, (b + 1)): # O número 0 e 1 não são primos
-    for i in range(a, c): # O primeiro número primo é 2
+    for i in range(a, c): # O primeiro número primo é 2, então a >= 2
         if c % i == 0:
             break
     else:
