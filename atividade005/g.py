b45f0f38e7a44879b8e1e9d06874ef5175664047
DianLuca <dianlucca@hotmail.com>
2024-05-31 15:52:04 -0300

    Validação inicial  da g.py

--- atividade005/g.py
+++ atividade005/g.py
@@ -15,9 +15,14 @@ print()
 a = int(input('Insira um número para começar..: '))
 b = int(input('Insira um número para finalizar: '))
 
+if a < 2: 
+    print(f'Não existe número primo de {a}, portando adotaremos o menor mais ' 
+          + f'próximo: 2')
+    a = 2
+
 for c in range(a, (b + 1)):
     for i in range(a, c):
         if c % i == 0:
             break
     else:
-        print(c)
+        print(c, end=' | ')
