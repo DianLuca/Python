2aca601109ce9331f466fb270892473a1fe0adc0
DianLuca <dianlucca@hotmail.com>
2024-05-31 11:16:06 -0300

    Desenvolvimento c.py

--- atividade005/c.py
+++ atividade005/c.py
@@ -1,2 +1,17 @@
-# Faça um programa que imprima os números no intervalo entre 1 e 10 
+# Faça um programa que imprima os números no intervalo entre 1 e 10
 # em ordem inversa.
+import os
+
+
+os.system('cls')
+
+print('PARA CONTAGEM REGRESSIVA')
+print()
+
+a = int(input('Insira o maior número: '))
+b = int(input('Insira o menor número: '))
+print()
+c = 0
+
+for c in range(a, (b - 1), -1):
+    print(c, end=' | ')
