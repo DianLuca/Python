bf606df2510006d9a107e9d3e8789a9c59fc2c2b
DianLuca <dianlucca@hotmail.com>
2024-05-31 15:39:04 -0300

    Desenvolvimento i.py

--- atividade005/i.py
+++ atividade005/i.py
@@ -1,3 +1,7 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Dian Luca Valente Nascimento
+# Data: 03/06/2024
 # Faça um algoritmo que imprima a frase "estou em looping" e, em seguida,
 # solicite ao usuário digitar uma letra. Caso a letra seja o “f" finalize
 # a aplicação. Do contrário, imprima novamente a mesma frase até que o 
