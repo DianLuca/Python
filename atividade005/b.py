ea9f49a9d5a079b7f4d70fd887efb7e967cdde5f
DianLuca <dianlucca@hotmail.com>
2024-05-30 22:51:59 -0300

    Desenvolvimento b.py

--- atividade005/b.py
+++ atividade005/b.py
@@ -1,2 +1,16 @@
 # Evolua o programa anterior para um código que pergunte ao usuário qual o 
 # intervalo que ele deseja ver  impresso.
+import os
+
+
+os.system('cls')
+
+print('QUAL O INTERVALO DESEJADO')
+a = int(input('Insira o valor inicial: '))
+b = int(input('Insira o valor fim....: '))
+
+c = 0
+
+for c in range(a, (b + 1)):
+    print(c, end =' - ')
+    c += 1
