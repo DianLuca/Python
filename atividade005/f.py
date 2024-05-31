d3cd591eca067c4176fddc073706bf7482551ce5
DianLuca <dianlucca@hotmail.com>
2024-05-31 14:39:18 -0300

    Desenvolvimento f.py

--- atividade005/f.py
+++ atividade005/f.py
@@ -1,1 +1,18 @@
 # Faça um programa que imprima os números primos entre 0 e 100.
+import os
+
+
+os.system('cls')
+
+print('NÚMEROS PRIMOS')
+print()
+
+a = int(input('Insira um número para começar: ')) # Inserir números maiores que 2
+b = int(input('Insira um número para finalizar:'))
+
+for c in range(a, (b + 1)): # O número 0 e 1 não são primos
+    for i in range(a, c): # O primeiro número primo é 2
+        if c % i == 0:
+            break
+    else:
+        print(c)
