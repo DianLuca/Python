d25997a4f4bf744815673120408b1d52f2d3a7d1
DianLuca <dianlucca@hotmail.com>
2024-05-31 11:52:35 -0300

    Desenvolvimento e.py

--- atividade005/e.py
+++ atividade005/e.py
@@ -1,2 +1,23 @@
 # Faça um programa que some a quantidade de números pares encontrados 
 # no intervalo entre 0 e 100.
+import os 
+
+
+os.system('cls')
+
+print('SOMATÓRIO DE NÚMEROS PARES')
+print()
+
+a = int(input('Insira o menor número: '))
+b = int(input('Insira o maior número: '))
+print()
+
+c = 0
+soma = 0
+
+for c in range(a, (b + 1), 2):
+    numero = c
+    
+    soma += numero
+    
+    print(soma, end=' | ')
