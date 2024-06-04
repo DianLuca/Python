48193cd1a4b6aa3cd1577aa4ab7d879927c1f143
DianLuca <dianlucca@hotmail.com>
2024-06-04 13:41:35 -0300

    Correção para entrada de passos ser inserida corretamente, além de ajustes para a execução do sistema.

--- testes/contagem.py
+++ testes/contagem.py
@@ -1,5 +1,6 @@
 # Apenas um exercício próprio para fixação
 import os
+import time
 
 
 os.system('cls')
@@ -7,10 +8,12 @@ os.system('cls')
 print('Exercício de contagem em um intervalo')
 print()
 print('-- SEJA BEM-VINDO --')
-init = input('Deseja iniciar o programa?[s - sim] ').lower()
+init = input('Deseja iniciar o programa?[s - sim] ').lower() # Input para iniciar o programa
 print()
 
 while init == 's':
+    print('Aguarde!')
+    time.sleep(3)
     print('Programa iniciado com sucesso')
     while True:
         a = int(input('Insira um número para iniciar o intervalo.: ')) # Variável para iniciar
@@ -18,36 +21,46 @@ while init == 's':
         c = int(input('Insira os passos: ')) # Passos a serem contados
         print()
 
-        if (a < b) and (c > 0): # crescente
-            for d in range(a, (b + 1), c):
-                print(d, end=' ')
-        elif (a > b) and (c > 0): # Para caso seja inserido o valor para contagem crescente de forma inválida
-            for d in range(b, (a + 1), c):
-                print(d, end=' ')
-        elif (a > b) and (c < 0): # decrescente
-            c = 1
-            for d in range(b, (a - 1), c):
-                print(d, end=' ')
-        elif (a < b) and (c < 0): # Para caso seja inserido o valor para contagem decrescente de forma inválida
-            c = 1
-            for d in range(a, (b - 1), c):
-                print(d, end=' ')
+        if c > 0: # contagem crecente
+            if (a < b):
+                for d in range(a, (b + 1), c):
+                    print(d, end=' ')
+            else: # Para caso seja inserido o valor para contagem crescente de forma inválida
+                for d in range(b, (a + 1), c):
+                    print(d, end=' ')
+        elif c < 0: # contagem decrescente
+            if (a > b): # Para caso seja inserido o valor para contagem decrescente de forma inválida
+                for d in range(a, (b - 1), c):
+                    print(d, end=' ')
+            else: 
+                for d in range(b, (a - 1), c):
+                    print(d, end=' ')
         else:
-            print(f'O intervalo possui apenas um número {a}')
+            print(f'O intervalo possui passo igual a {c}, insira um passo' 
+                  + f'maior ou menor para executar a operação.')
         print()
         print('Fim do intervalo!')
 
-        reset = input('Deseja contar novamente? [s - sim] ').lower()
-        if reset == 's':
+        reset = input('Deseja contar novamente? [s - sim] ').lower() # Input para parada de contagem
+        if reset != 's':
+            fechar = input('Deseja finalizar o programa? [s - sim] ').lower() # Input para encerrar o sistema
             os.system('cls')
+            if fechar == 's':
+                os.system('cls')
+                break
         else:
-            break
+            os.system('cls')
+    print('Fim do programa!')
+    time.sleep(3)
+    os.system('cls')
+    break      
 else:
-    print('Fim do programa')
+    print('Fim do programa!')
+    time.sleep(3)
     os.system('cls')
 
 
 # Adicionar validação para o caso de C < 0 e dividir as condições e o mesmo 
-# para caso o valor seja = 0 
-# Validar as entradas de valor, para que todos sejam valores númericos
+# para caso o valor seja = 0 - REALIZADO
+# Validar as entradas de valor, para que todos sejam valores válidos para às entradas
 # Mais ????
