bd60ce29e63f440d8152479a775ac44a07d74205
DianLuca <dianlucca@hotmail.com>
2024-06-05 12:05:49 -0300

    Validação dos valores inseridos realizada

--- testes/contagem.py
+++ testes/contagem.py
@@ -1,7 +1,8 @@
 # Apenas um exercício próprio para fixação
 import os
-import time # Para adicionar tempo para execução do programa
-import numpy as np # Para me permitir utilizar valores decimais para contagem dos intervalos
+import time  # Para adicionar tempo para execução do programa
+# Para me permitir utilizar valores decimais para contagem dos intervalos
+import numpy as np
 
 
 os.system('cls')
@@ -9,7 +10,8 @@ os.system('cls')
 print('Exercício de contagem em um intervalo')
 print()
 print('-- SEJA BEM-VINDO --')
-init = input('Deseja iniciar o programa?[s - sim] ').lower() # Input para iniciar o programa
+# Input para iniciar o programa
+init = input('Deseja iniciar o programa?[s - sim] ').lower()
 print()
 
 while init == 's':
@@ -17,34 +19,46 @@ while init == 's':
     time.sleep(3)
     print('Programa iniciado com sucesso')
     while True:
-        a = float(input('Insira um número para iniciar o intervalo.: ')) # Variável para iniciar
-        b = float(input('Insira um número para encerrar o intervalo: ')) # Variável para finalizar
-        c = float(input('Insira os passos: ')) # Passos a serem contados
+        a = input('Insira um número para iniciar o intervalo.: ') # Variável para iniciar
+        b = input('Insira um número para encerrar o intervalo: ') # Variável para finalizar
+        c = input('Insira os passos: ')  # Passos a serem contados
         print()
 
-        if c > 0: # contagem crecente
-            if (a < b):
-                for d in np.arange(a, (b + .1), c): # .1 inserido para evitar erros na contagem, 
-                    print(round(d,4), end=' ')      # mas apresenta erro em contas decimais
-            else: # Para caso seja inserido o valor para contagem crescente de forma inválida
-                for d in np.arange(b, (a + .1), c): 
-                    print(round(d,4), end=' ')
-        elif c < 0: # contagem decrescente
-            if (a > b): 
-                for d in np.arange(a, (b - .1), c):
-                    print(round(d,4), end=' ')
-            else: # Para caso seja inserido o valor para contagem decrescente de forma inválida
-                for d in np.arange(b, (a - .1), c):
-                    print(round(d,4), end=' ')
+        # Validação dos valores inseridos
+        if not (a.isnumeric() and b.isnumeric() and c.isnumeric()):
+            print('O item possui um caracter inválido! Insira apenas números.')
         else:
-            print(f'O intervalo possui passo igual a {c}, insira um passo' 
+            a = float(a)
+            b = float(b) # Casting para poder usar as variáveis de forma correta
+            c = float(c)
+            
+            if c > 0:  # contagem crecente
+                if (a < b):
+                    # .1 inserido para evitar erros na contagem,
+                    for d in np.arange(a, (b + .1), c):
+                        # mas apresenta erro em contas decimais
+                        print(round(d, 4), end=' ')
+                else:  # Para caso seja inserido o valor para contagem crescente de forma inválida
+                    for d in np.arange(b, (a + .1), c):
+                        print(round(d, 4), end=' ')
+            elif c < 0:  # contagem decrescente
+                if (a > b):
+                    for d in np.arange(a, (b - .1), c):
+                        print(round(d, 4), end=' ')
+                else:  # Para caso seja inserido o valor para contagem decrescente de forma inválida
+                    for d in np.arange(b, (a - .1), c):
+                        print(round(d, 4), end=' ')
+            else:
+                print(f'O intervalo possui passo igual a {c}, insira um passo'
                   + f'maior ou menor para executar a operação.')
         print()
         print('Fim do intervalo!')
 
-        reset = input('Deseja contar novamente? [s - sim] ').lower() # Input para parada de contagem
+        # Input para parada de contagem
+        reset = input('Deseja contar novamente? [s - sim] ').lower()
         if reset != 's':
-            fechar = input('Deseja finalizar o programa? [s - sim] ').lower() # Input para encerrar o sistema
+            # Input para encerrar o sistema
+            fechar = input('Deseja finalizar o programa? [s - sim] ').lower()
             os.system('cls')
             if fechar == 's':
                 os.system('cls')
@@ -54,14 +68,14 @@ while init == 's':
     print('Fim do programa!')
     time.sleep(3)
     os.system('cls')
-    break      
+    break
 else:
     print('Fim do programa!')
     time.sleep(3)
     os.system('cls')
 
 
-# Adicionar validação para o caso de C < 0 e dividir as condições e o mesmo 
+# Adicionar validação para o caso de C < 0 e dividir as condições e o mesmo
 # para caso o valor seja = 0 - REALIZADO
 # Validar as entradas de valor, para que todos sejam valores válidos para às entradas
 # Mais ????
