d154e3052a0112f6b3af9c8e28d319ea969d6572
DianLuca <dianlucca@hotmail.com>
2024-06-05 15:16:04 -0300

    Desenvolvimento da questão j.py

--- atividade005/j.py
+++ atividade005/j.py
@@ -6,16 +6,21 @@ import os
 
 os.system('cls')
 
-print('NÚMEROS ÍMPARES EM UM INTERVALO E SUA QUANTIDADE')
-inicio = int(input('Insira um valor inteiro para iniciar o intervalo: '))
+print('NÚMEROS ÍMPARES EM UM INTERVALO E SUA QUANTIDADE E SOMA TOTAL')
+inicio = int(input('Insira um valor inteiro para iniciar o intervalo..: '))
 final = int(input('Insira um valor inteiro para finalizar o intervalo: '))
-
+print()
 c_impar = 0
+somatorio = 0
 
 for c in range(inicio, (final +1)):
     
     if (c % 2 != 0) == True:
-        print(c, end=' ')
-        
-    
-    
+        print(c, end=' | ')
+        c_impar += 1
+        somatorio += c
+                
+print('\n')       
+print(f'Existe {c_impar} número(s) ímpar(es) nesse intervalo.')
+print()
+print(f'A soma de todos os valores é igual a: {somatorio}')
