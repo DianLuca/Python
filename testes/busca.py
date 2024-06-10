010bdd556058ee2f2779fe7f163f802a3367fea8
DianLuca <dianlucca@hotmail.com>
2024-06-10 11:50:43 -0300

    Adicionando padronização e comentário

--- testes/busca.py
+++ testes/busca.py
@@ -18,9 +18,14 @@ encontrado = False  # Variável para controlar se o item foi encontrado
 
 # Verifica se os valores associados à chave "produto" começam com o prefixo especificado
 for item in estoque:
-    if busca.lower() in item["produto"].lower():  # Usamos lower() para tornar a comparação insensível a maiúsculas e minúsculas
-        print(f'Nome do produto: {item["produto"]} | Quantidade: {item["quantidade"]} | Valor: {item["preco_unitario"]}')
+    # Usamos lower() para tornar a comparação insensível a maiúsculas e minúsculas
+    if busca.lower() in item["produto"].lower():
+        print(f'Nome do produto: {item["produto"]} | '
+              f'Quantidade: {item["quantidade"]} | '
+              f'Valor: {item["preco_unitario"]}')
         encontrado = True
 
 if not encontrado:
     print('Nenhum item encontrado com essa palavra nos nomes dos produtos.')
+
+# Para o caso de muitos itens devo procurar outras formas, essa busca é limitada
