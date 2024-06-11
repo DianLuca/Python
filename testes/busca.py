48dcaf4abd6d8e93c78b94eff5b79a0942c9b585
DianLuca <dianlucca@hotmail.com>
2024-06-11 13:26:39 -0300

    Adicionado opções de filtragem e comentários

--- testes/busca.py
+++ testes/busca.py
@@ -7,9 +7,29 @@ estoque = [
     {"produto": "Camiseta", "quantidade": 50, "preco_unitario": 25.99},
     {"produto": "Calça", "quantidade": 30, "preco_unitario": 39.99},
     {"produto": "Blusa", "quantidade": 20, "preco_unitario": 19.99},
-    {"produto": "Calça Jeans", "quantidade": 30, "preco_unitario": 39.99},
+    {"produto": "Calça Jeans", "quantidade": 35, "preco_unitario": 45.99},
     {"produto": "Alça Jeans", "quantidade": 30, "preco_unitario": 39.99}
 ]
+
+# Ordenando - Criar após uma forma de escolher como será filtrado - produto, quantidade e preço
+# os.system('cls')
+filtro = input('Filtrar por: 1 - Produto | 2 - Quantidade | 3 - Preço | ').lower().strip()
+
+if filtro == '1' or filtro == 'produto':
+    filtro = 'produto'
+elif filtro == '2' or filtro == 'quantidade':
+    filtro = 'quantidade'
+elif filtro == '3' or filtro == 'preço':
+    filtro = 'preco_unitario'
+else: # Caso não seja definido nenhum ou algo diferente este será o padrão do sistema
+    filtro = 'produto'
+    
+
+lista_crescente = sorted(estoque, key=lambda x: x[filtro]) # sorted pega a lista e retorna ordenada de acordo com o que foi solicitado
+for item in lista_crescente:                               # lambda pega a lista completa e difine X para usar como argumento, permitindo filtrar pelo que foi solicitado
+    print(f'Nome do produto: {item["produto"]} | '
+              f'Quantidade: {item["quantidade"]} | '
+              f'Valor: {item["preco_unitario"]}')
 
 busca = input('Busca: ').strip()
 itens = 0
@@ -24,7 +44,6 @@ for item in estoque:
               f'Quantidade: {item["quantidade"]} | '
               f'Valor: {item["preco_unitario"]}')
         encontrado = True
-
 if not encontrado:
     print('Nenhum item encontrado com essa palavra nos nomes dos produtos.')
 
