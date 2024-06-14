c2b7e0d575ed92c9133cf85f45361a1cf352cfab
DianLuca <dianlucca@hotmail.com>
2024-06-14 15:02:51 -0300

    Desenvolvimento da atividade d.py

--- atividade007/d.py
+++ atividade007/d.py
@@ -3,3 +3,35 @@
 # Autor: Dian Luca Valente Nascimento
 # Data: 17/06/2024 
 # Utilizando o exercício anterior, coloque todas as listas em ordem crescente de valor
+import os
+import random # Para gerar números aleatórios
+
+
+os.system('cls')
+
+inicio = int(input('Insira um valor inteiro para sortear: '))
+final = int(input('Insira outro valor inteiro para sortear: '))
+
+# Gera 50 números aleatórios na lista. "_" (Valor vazio) é utilizado, pois não importa os valores 
+# gerados no intervalo, só é necessários que eles sejam inseridos.
+lista_numeros = [random.randint(inicio,final) for _ in range(50)] 
+
+
+lista_fatiada = []
+
+for i in range(1, 51, 10): 
+    if i + 10 <= len(lista_numeros): # Enquanto não for menor ou igual a 10 não criará uma sublista
+        lista = lista_numeros[i:i+10] # Verifica o número de itens na lista e quebra caso aconteça
+        lista_fatiada.append(lista) # Insere na lista
+    else:
+        lista = lista_numeros[i:]
+        lista_fatiada.append(lista)
+
+
+print()
+
+
+for indice, lista in enumerate(lista_fatiada, start= 1): # Cria uma lista enumerada com 10 itens
+    lista_ordenada = sorted(lista)
+    print(f'{indice}: {lista_ordenada}')
+    
