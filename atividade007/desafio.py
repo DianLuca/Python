d06311e485bd766584f6b6a52ac74dbe1d8b0c34
DianLuca <dianlucca@hotmail.com>
2024-06-14 15:08:05 -0300

    Criação do arquivo desafio.py e seu cabeçalho

---
+++ atividade007/desafio.py
@@ -0,0 +1,7 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Dian Luca Valente Nascimento
+# Data: 17/06/2024
+# DESAFIO: Construa um código para exemplificar um CRUD. Não é permitido funções ou validações try exception.
+
+# 👀👀 ESTAMOS DE OLHO 👀👀
