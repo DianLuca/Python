1b9466ca495930a8e655839ffa3e211ddf3d20c1
DianLuca <dianlucca@hotmail.com>
2024-06-28 13:42:41 -0300

    Desenvolvimento da atividade a.py

--- atividade009/a.py
+++ atividade009/a.py
@@ -4,3 +4,29 @@
 # Data: 01/07/2024
 
 # A - Faça um programa para criar um dicionário com 4 elementos.
+import os
+
+
+os.system('cls')
+
+elementos = {}
+
+print('ADICIONADO ELEMENTOS')
+
+num_item = input('Quantos itens deseja inserir? ')
+
+num_item = int(num_item)
+
+# Adicionando o número de itens desejado
+for c in range(num_item):
+    print(f'Adicionando item {c + 1}')
+    chave = input('Digite a chave: ')
+    valor = input('Digite o valor: ')
+    elementos[chave] = valor
+
+# Exibindo valores adicionados
+print(elementos)
+  
+
+for chave, valor in elementos.items():
+    print(f'{chave.capitalize()}: {valor}', end=' | ')
