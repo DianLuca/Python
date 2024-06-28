204a14ad6e74a40de54543092919614cc9015dd3
DianLuca <dianlucca@hotmail.com>
2024-06-28 14:07:56 -0300

    Desenvolvimento da atividade b.py

--- atividade009/b.py
+++ atividade009/b.py
@@ -5,3 +5,29 @@
 
 # B - Utilizando o exercício anterior,  adicione mais 2 elementos ao 
 # dicionário.
+import os
+
+
+os.system('cls')
+
+elementos = {}
+
+while True:
+    num_item = input('Quantos itens deseja adicionar: ')
+    num_item = int(num_item)
+
+    for c in range(num_item):
+        print(f'Adicionando item {c + 1}')
+        chave = input('Insira uma chave: ')
+        valor = input('Insira um valor: ')
+        os.system('cls')
+        elementos[chave] = valor
+        
+    print(elementos)
+
+    print('Elementos no dicionário:')
+    for chave, valor in elementos.items():
+        print(f'{chave}: {valor}', end=' | ')
+        
+    input('\nEnter para continuar')
+    os.system('cls')
