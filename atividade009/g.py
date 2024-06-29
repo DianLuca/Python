9779328600c99e9da1a7d0977ac9a5e0359e7b2e
DianLuca <dianlucca@hotmail.com>
2024-06-28 21:32:55 -0300

    Desenvolvimento da atividade g.py

--- atividade009/g.py
+++ atividade009/g.py
@@ -5,3 +5,29 @@
 
 # G - Faça um programa para cadastrar os alunos de uma escola. Para os campos,
 # tome como referência o nome do aluno, data de nascimento e matrícula.
+import os
+
+
+os.system('cls')
+cadastro = []
+alunos = {}
+
+while True:
+    print('CADASTRO DE ALUNOS')
+    
+    num_item = input('Quantos itens deseja adicionar: ')
+    num_item = int(num_item)
+    
+    for c in range(num_item):
+        print(f'Adicionando o {c + 1}º aluno:')
+        alunos['nome'] = input('Insira o nome: ')
+        alunos['data_nascimento'] = input('Insira a data de nascimento: ')
+        alunos['matricula'] = input('Insira a matricula: ')
+        os.system('cls')
+        cadastro.append(alunos.copy())
+        
+    for aluno in cadastro:
+        for chave, valor in aluno.items():
+            print(f'{chave}: {valor}', end=' | ')
+        print()
+        
