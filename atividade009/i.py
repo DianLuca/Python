30de1d0b853dd4c03f416413f1800aa9f228f13a
DianLuca <dianlucca@hotmail.com>
2024-06-29 17:49:05 -0300

    Desenvolvimento da atividade i.py

--- atividade009/i.py
+++ atividade009/i.py
@@ -6,3 +6,43 @@
 # I - Faça um programa para criar um dicionário com 4 elementos. Depois 
 # imprima a lista completa, delete o último elemento e mostre uma 
 # listagem nova.
+import os
+
+
+os.system('cls')
+
+lista_elementos = []
+elementos = {}
+
+while True:
+    print('Adicionando elementos.')
+    num_item = input('Quantos items deseja adicionar? ')
+    num_item = int(num_item)
+    
+    for c in range(num_item):
+        print(f'Adicionando o {c + 1}º elemento:')
+        elementos['nome'] = input('Insira um nome: ')
+        elementos['idade'] = input('Insira a idade: ')
+        os.system('cls')
+        lista_elementos.append(elementos.copy())
+    
+    for elemento in lista_elementos:
+        for chave, valor in elemento.items():
+            print(f'{chave}: {valor}', end=' | ')
+        print()
+        
+    print()
+    print('O último item inserido será removido.')
+    input('Pressione Enter para continuar. ')
+    os.system('cls')
+    
+    lista_elementos.pop()
+            
+    for elemento in lista_elementos:
+        for chave, valor in elemento.items():
+            print(f'{chave}: {valor}', end=' | ')
+        print()
+    
+    print()
+    input('Pressione Enter para continuar. ')
+    os.system('cls')
