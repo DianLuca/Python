52e532bac944089331edf882fe1b006e735127bf
DianLuca <dianlucca@hotmail.com>
2024-06-29 21:26:23 -0300

    Desenvolvimento da atividade k.py

--- atividade009/k.py
+++ atividade009/k.py
@@ -1,8 +1,32 @@
 # Curso de Desenvolvimento de Sistemas
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
-# Data: 01/07/2024 
+# Data: 01/07/2024
 
 # K - Faça um programa que peça continuamente o nome e a idade de uma pessoa.
 # Caso o usuário digite a idade 999, o programa será finalizado e executará
 # uma impressão com os nomes cadastrados.
+import os
+
+
+os.system('cls')
+
+pessoas = {}
+
+while True:
+    print('Adicionando pessoas:')
+    print()
+    chave = input('Insira um nome: ')
+    valor = input('Insira a idade (999 - Encerra o programa): ')
+    pessoas[chave] = valor
+
+    if valor == '999':
+        os.system('cls')
+        print('Todos as pessoas na lista:')
+        for chave, valor in pessoas.items():
+            print(f'{chave.capitalize()}: {valor}')
+        print()
+        input('Pressione Enter para encerrar o programa. ')
+        break
+    else:
+        os.system('cls')
