b5a38cc162eb5d12116383e317db1f047030ccd0
DianLuca <dianlucca@hotmail.com>
2024-07-10 20:40:57 -0300

    Criando o arquivo aula013_return.py

---
+++ aula013_def/aula013_return.py
@@ -0,0 +1,35 @@
+import os
+
+
+os.system('cls')
+
+# Definindo uma função
+def calcular_velocidade_media(distancia, tempo):
+    #Vm = ΔS/Δt
+    if distancia < 1000:
+        dividendo_distancia = 1
+        medida = 'km'
+    else:
+        dividendo_distancia = distancia / 1000
+        medida = 'm'
+        
+    if tempo < 60:
+        dividendo_tempo = 1
+        unidade_tempo = 'h'
+    else:
+        dividendo_tempo = tempo / 60
+        unidade_tempo = 's'
+        
+    velocidade_media = (distancia / dividendo_distancia) / (tempo / dividendo_tempo)
+    return velocidade_media, medida, unidade_tempo
+
+distancia = float(input('Digite a distância percorrida (em km): '))
+tempo = float(input('Digite a tempo gasto (em horas): '))
+unidade = input('Você deseja fazer utilizar kh/m ou m/s: ')
+
+
+# Calcular a velocidade média usando a função criada
+velocidade = calcular_velocidade_media(distancia, tempo)
+
+# Exibir o resultado
+print(f'A velocidade média é {velocidade:.2f}.')
