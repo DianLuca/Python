30e092698a37437d27c1db084374556a118cbcac
DianLuca <dianlucca@hotmail.com>
2024-07-11 11:18:13 -0300

    Desenvolvendo...

--- aula013_def/aula013_return.py
+++ aula013_def/aula013_return.py
@@ -7,20 +7,16 @@ os.system('cls')
 def calcular_velocidade_media(distancia, tempo):
     #Vm = ΔS/Δt
     if distancia < 1000:
-        dividendo_distancia = 1
-        medida = 'km'
-    else:
         dividendo_distancia = distancia / 1000
-        medida = 'm'
+    else:
+        dividendo_distancia = distancia
         
     if tempo < 60:
-        dividendo_tempo = 1
-        unidade_tempo = 'h'
+        dividendo_tempo = tempo
     else:
         dividendo_tempo = tempo / 60
-        unidade_tempo = 's'
         
-    velocidade_media = (distancia / dividendo_distancia) / (tempo / dividendo_tempo)
+    velocidade_media = (dividendo_distancia) / (dividendo_tempo)
     return velocidade_media
 
 
@@ -32,4 +28,4 @@ tempo = float(input('Digite a tempo gasto (em horas): '))
 velocidade = calcular_velocidade_media(distancia, tempo)
 
 # Exibir o resultado
-print(f'A velocidade média é {velocidade:.2f}.')
+print(f'A velocidade média é {velocidade:.2f}km/h.')
