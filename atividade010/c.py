4a68eec38fbfcd1b990f2f5f3ed931070ea1ac62
DianLuca <dianlucca@hotmail.com>
2024-07-11 15:09:59 -0300

    Desenvolvimento da atividade c.py

--- atividade010/c.py
+++ atividade010/c.py
@@ -7,3 +7,26 @@
 # dicionário. Se estiver cadastrado, imprima o nome desse aluno e o resto dos
 # seus dados. O dicionário deverá conter nome, matrícula e a data de 
 # nascimento do aluno.
+import os
+
+
+os.system('cls')
+
+cadastro = [
+    {'nome': 'Dian', 'matricula': '007', 'data_nascimento': '00/00/0000'}
+]
+aluno = {}
+
+def encontrar():
+    aluno = input('Insira o nome para ser verificado: ')
+    for alunos in cadastro:
+        if aluno in alunos['nome']:
+            for k, v in alunos.items():
+                print(f'{k}: {v}', end=' | ')
+        else:
+            print(f'O {aluno} não está presente')
+
+while True:
+    encontrar()
+    print()
+    input('Enter para continuar ')
