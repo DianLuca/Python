3b9cfbc3597cb34bfca295c9c1a2f63b25f2020a
DianLuca <dianlucca@hotmail.com>
2024-07-11 19:36:08 -0300

    Desenvolvimento da atividade e.py

--- atividade010/e.py
+++ atividade010/e.py
@@ -5,3 +5,20 @@
 
 # Crie uma função que receba a altura e o peso de uma pessoa. Depois retorne 
 # o seu IMC.
+import os
+
+
+os.system('cls')
+
+def calcular(altura, peso):
+    altura = float(altura)
+    peso = float(peso)
+    calcula_imc = (peso / (altura**2))
+    return calcula_imc
+
+peso = input('Insira o seu peso: ')
+altura = input('Insira o seu altura: ')
+
+imc = calcular(altura, peso)
+
+print(f'Altura: {altura} m \nPeso: {peso} Kg \nIMC: {imc:.2f}')
