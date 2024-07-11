d7519fae8175b90c81219885e59648267298b5f8
DianLuca <dianlucca@hotmail.com>
2024-07-11 19:04:46 -0300

    Desenvolvimento da atividade d.py

--- atividade010/d.py
+++ atividade010/d.py
@@ -5,3 +5,19 @@
 
 # Crie uma função que receba uma temperatura em fahrenheit e retorne o valor
 # em graus célsius.
+import os
+
+
+os.system('cls')
+
+
+def converter(fahrenheit):
+    fahrenheit = float(fahrenheit)
+    fahrenheit_celsius = (fahrenheit - 32) * (5/9)
+    return fahrenheit_celsius
+
+
+fahrenheit = input('Insira a temperatura em fahrenheit: ')
+celcius = converter(fahrenheit)
+
+print(f'{fahrenheit}°F = {celcius}°C')
