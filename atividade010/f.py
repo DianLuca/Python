f5f0fece1c12680ed126ca62729d4e9f07f8ccb6
DianLuca <dianlucca@hotmail.com>
2024-07-11 21:15:00 -0300

    Desenvolvimento da atividade f.py

--- atividade010/f.py
+++ atividade010/f.py
@@ -9,3 +9,21 @@
 # - Sua função deverá criar um dicionário contendo chave e valor utilizando 
 # como base essas duas listas. Depois, seu programa deverá imprimir esse 
 # dicionário utilizando uma estrutura de repetição for.
+import os
+
+
+os.system('cls')
+
+lista_1 = ['nome', 'peso', 'idade']
+lista_2 = ['John', '40', '1.8']
+lista_unida = []
+
+def unir():
+    saida = dict(zip(lista_1, lista_2))
+    lista_unida.append(saida)
+    for k, v in saida.items():
+        print(f'{k}: {v}', end=' | ')
+    print()
+
+unir()
+input('continue')
