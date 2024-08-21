bb17f192c846cdd39d6d3b9d4acb981c6678ec0f
DianLuca <dianlucca@hotmail.com>
2024-08-20 21:35:59 -0300

    Criando aula017_conta_bancaria.py

---
+++ Orientado_Objeto/aula017_classe/aula017_conta_bancaria.py
@@ -0,0 +1,33 @@
+import os
+
+
+class ContaBancaria:
+    def __init__(self, numero_conta, nome_titular, saldo, agencia, tipo_conta):
+        self.numero_conta = numero_conta
+        self.nome_titular = nome_titular
+        self.saldo = saldo
+        self.agencia = agencia
+        self.tipo_conta = tipo_conta
+        
+# Solicitando entrada de dados do usuário
+os.system('cls')
+print('-' * 70)
+numero_conta = input('Digite o número da conta: ')
+nome_titular = input('Digite o nome do titular: ')
+saldo = float(input('Digite o saldo inicial: '))
+agencia = input('Digite a agência: ')
+tipo_conta = input('Digite o tipo da conta: ')
+
+# Criando um objeto de classe ContaBancaria com os dados fornecidos pelo usuário
+conta = ContaBancaria(numero_conta, nome_titular, saldo, agencia, tipo_conta)
+
+# Acessando e imprimindo atributos do objeto
+print('-' * 70)
+print('\nInformações da Conta Bancária: ')
+print('=' * 70)
+print(f'Número da Conta: {conta.numero_conta}')
+print(f'Nome do Titular: {conta.nome_titular}')
+print(f'Saldo: {conta.saldo}')
+print(f'Agência: {conta.agencia}')
+print(f'Tipo de Conta: {conta.numero_conta}')
+print('-' * 70)
