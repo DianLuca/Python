1ce098b47aa472d03c64c06a19cd94edb97034ad
DianLuca <dianlucca@hotmail.com>
2024-09-03 14:18:16 -0300

    Desenvolvimento da questão f.py

--- Orientado_Objeto/atividade001/f.py
+++ Orientado_Objeto/atividade001/f.py
@@ -4,3 +4,36 @@
 # Data: 03/09/2024
 # Faça um programa que receba um número qualquer e calcule o dobro e o triplo 
 # desse número.
+import os
+
+
+class Operacao:
+    def __init__(self, valor):
+        self.valor = valor
+    
+    def multiplicar(self, valor):
+        try:
+            valor = float(valor)
+            dobro = valor * 2
+            triplo = valor * 3
+            return f'O dobro e o triplo do valor {valor} são respectivamente: {dobro} e {triplo}'
+        except (TypeError,ValueError):
+            return 'Insira apenas valores numéricos! '
+
+while True:
+    os.system('cls')
+    print('-' * 70)
+    print('ENTRADA DE DADOS')
+    print('-' * 70)
+
+    # Entrada
+    valor = input('Insira qualquer valor (s - Sair): ')
+    if valor == 's':
+        break
+    else:
+        operacao = Operacao(valor)
+        resultado = operacao.multiplicar(valor)
+
+    print('-' * 70)
+    input(f'--- RESULTADO --- \n{resultado}\n' + '=' * 70)
+    
