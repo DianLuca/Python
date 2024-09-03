4fdb289f99f4304189197d8823a482c63c680862
DianLuca <dianlucca@hotmail.com>
2024-09-03 15:36:41 -0300

    Desenvolvimento das questões i.py e j.py

--- Orientado_Objeto/atividade001/i.py
+++ Orientado_Objeto/atividade001/i.py
@@ -2,5 +2,44 @@
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
 # Data: 03/09/2024
-# Faça um programa que receba um valor em reais, depois calcule quantos 
+# Faça um programa que receba um valor em reais, depois calcule quantos
 # dólares daria para comprar com esse valor.
+import os
+
+
+valor_dolar = 5.64  # Cotação de 19/04/2024
+
+
+class Cambio:
+    def __init__(self, moeda):
+        self.moeda = moeda
+
+    def conversor(self, moeda):
+        try:
+            moeda = float(moeda)
+            conversao_dolar_real = valor_dolar * moeda
+            conversao_real_dolar = moeda / valor_dolar
+            return f'$ {moeda:.2f} dólar(es) corresponde a: R$ {conversao_dolar_real:.2f} \nR$ {moeda:.2f} real(is) correspondem a: $ {conversao_real_dolar:.2f}'
+
+        except (TypeError, ValueError):
+            return 'Insira apenas valores numéricos! '
+
+
+while True:
+    os.system('cls')
+
+    print('-' * 70)
+    print('ENTRADA DE DADOS')
+    print('-' * 70)
+
+    # Entrada
+    valor_real = input('Insira um valor em reais para conversão (s - Sair): ')
+    if valor_real == 's':
+        break
+    else:
+        cambio = Cambio(valor_real)
+        valor_convertido = cambio.conversor(valor_real)
+
+    print('-' * 70)
+    print('--- CONVERSÃO DE VALORES')
+    input(f'{valor_convertido}\n' + '=' * 70)
--- Orientado_Objeto/atividade001/j.py
+++ Orientado_Objeto/atividade001/j.py
@@ -3,3 +3,40 @@
 # Autor: Dian Luca Valente Nascimento
 # Data: 03/09/2024
 # Faça um programa com entrada de dados para calcular o perímetro de um retângulo.
+import os
+
+
+class Retangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+        
+    def calcular_perimetro(self, base, altura):
+        try:
+            base = float(base)
+            altura = float(altura)
+            perimetro_retangulo = 2*(base + altura)
+            return f'O perímetro de um retângulo de base: {base:.2f} cm e altura: {altura:.2f} cm é de: {perimetro_retangulo:.2f} cm'
+        except (TypeError, ValueError):
+            return 'Insira apenas valores numéricos! '
+
+
+while True:  
+    os.system('cls')
+
+    print('-' * 70)
+    print('ENTRADA DE DADOS')
+    print('-' * 70)
+
+    menu =  input('1 - Calcular o perímetro de um retângulo | 0 - Sair: ')
+    if menu == '1':
+        base = input('Insira a base do retângulo (em cm): ')
+        altura = input('Insira a altura do retângulo (em cm): ')
+        retangulo = Retangulo(base, altura)
+        perimetro = retangulo.calcular_perimetro(base, altura)
+        print('-' * 70)
+        print('-- RESULTADO --')
+        input(f'{perimetro}\n' + '=' * 70)
+        
+    elif menu == '0':
+        break
