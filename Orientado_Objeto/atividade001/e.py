8ff88ef716e87b40745e0c4176807f526d3a53fe
DianLuca <dianlucca@hotmail.com>
2024-09-03 13:59:16 -0300

    Desenvolvimento da questão e.py

--- Orientado_Objeto/atividade001/e.py
+++ Orientado_Objeto/atividade001/e.py
@@ -2,5 +2,37 @@
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
 # Data: 03/09/2024
-# Faça um programa que receba um número inteiro e mostre o sucessor 
+# Faça um programa que receba um número inteiro e mostre o sucessor
 # e antecessor.
+import os
+
+
+class Adjacentes:
+    def __init__(self, valor):
+        self.valor = valor
+
+    def sucessor_antecessor(self, valor):
+        try:
+            valor = float(valor)
+            antecessor = valor - 1
+            sucessor = valor + 1
+            return f'O antessessor e o sucessor do valor: {valor}, são os seguintes respectivamentes: {antecessor} e {sucessor}'
+        except (TypeError, ValueError):
+            return 'Insira apenas valores numéricos! '
+
+
+while True:
+    os.system('cls')
+
+    print('-' * 70)
+    print('ENTRADA DE DADOS')
+    print('-' * 70)
+
+    # Entrada
+    valor = input('Insira um valor (s - Sair): ')
+    if valor == 's':
+        break
+    else:
+        adjacentes = Adjacentes(valor)
+        resultado = adjacentes.sucessor_antecessor(valor)
+        input(resultado)
