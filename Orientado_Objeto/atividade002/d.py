23da5b343aff747bd3b52e3b051306747af3ad60
DianLuca <dianlucca@hotmail.com>
2024-09-06 21:07:12 -0300

    Desenvolvimento da atividade d.py

--- Orientado_Objeto/atividade002/d.py
+++ Orientado_Objeto/atividade002/d.py
@@ -6,23 +6,31 @@
 import os
 
 
+class Numeros:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+
+class Pares(Numeros):
+    def par(self):
+        c_par = 0
+        for c in range(self.inicio, (self.fim + 1)):
+            if (c % 2 == 0):
+                c_par += 1
+                print(c, end=' | ')
+        return c_par
+
+
 os.system('cls')
 
 print('MOSTRADOR DE NÚMEROS PARES')
 print()
 print('INSIRA O INTERVALO DESEJADO:')
-a = int(input('Insira o menor número: '))
-b = int(input('Insira o maior número: '))
+inicio = int(input('Insira o menor número: '))
+fim = int(input('Insira o maior número: '))
 print()
-
-c = 0
-c_par = 0
-
-print(f'Os números pares no intervalo entre {a} e {b}, serão: ')
-
-for c in range(a, b + 1):
-    if (c % 2 == 0) == True:
-        c_par += 1
-        print(c, end=' | ')
-print()
+print(f'Os números pares no intervalo entre {inicio} e {fim}, serão: ')
+numeros = Pares(inicio, fim)
+c_par = numeros.par()
 print(f'\nOs existe {c_par} número(s) par(es) nesse intervalo.')
