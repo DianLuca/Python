7512aa939c8cd155dc73a11522c411d013bce124
DianLuca <dianlucca@hotmail.com>
2024-09-09 13:30:41 -0300

    Desenvolvimento da atividade h.py

--- Orientado_Objeto/atividade002/h.py
+++ Orientado_Objeto/atividade002/h.py
@@ -8,19 +8,41 @@
 import os
 
 
+class Numeros:
+    def __init__(self, inicio, fim, primeiro, segundo, terceiro):
+        self.inicio = inicio
+        self.fim = fim
+        self.primeiro = primeiro
+        self.segundo = segundo
+        self.terceiro = terceiro
+
+    def exibir(self, apresentar):
+        print(apresentar, end=' | ')
+
+
+class Intervalo(Numeros):
+    def ignorar(self):
+        try:
+            for c in range(int(self.inicio), (int(self.fim) + 1)):
+                if ((c == int(self.primeiro)) or (c == int(self.segundo)) or (c == int(self.terceiro))):
+                    continue
+                self.exibir(c)
+        except (TypeError, ValueError):
+            print('Insira apenas valores numéricos! ')
+
+
 os.system('cls')
 
 print('INSIRA UM INTERVALO E SELECIONE 3 NÚMEROS PARA IGNORAR')
 print()
 print('-- INTERVALO --')
-inicial = int(input('Insira um número para iniciar.: '))
-final = int(input('Insira um número para encerrar: '))
+inicial = input('Insira um número para iniciar.: ')
+final = input('Insira um número para encerrar: ')
 print('-- NÚMEROS IGNORADOS --')
-primeiro = int(input('Insira o 1º número a ser ignorado: '))
-segundo = int(input('Insira o 2º número a ser ignorado: '))
-terceiro = int(input('Insira o 3º número a ser ignorado: '))
+primeiro = input('Insira o 1º número a ser ignorado: ')
+segundo = input('Insira o 2º número a ser ignorado: ')
+terceiro = input('Insira o 3º número a ser ignorado: ')
 
-for c in range(inicial, (final + 1)):
-    if ((c == primeiro) or (c == segundo) or (c == terceiro)):
-        continue
-    print(c, end=' | ')
+intervalo = Intervalo(inicial, final, primeiro, segundo, terceiro)
+print('A lista com os valores ignorados:')
+intervalo.ignorar()
