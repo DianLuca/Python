6298a706e94e1dddd3cd5c01f394fc1a817bf889
DianLuca <dianlucca@hotmail.com>
2024-09-09 14:20:20 -0300

    Desenvolvimento da atividade j.py

--- Orientado_Objeto/atividade002/j.py
+++ Orientado_Objeto/atividade002/j.py
@@ -2,29 +2,42 @@
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
 # Data: 03/09/2024
-# Crie um programa que realiza a contagem de 1 até 100, usando apenas de 
+# Crie um programa que realiza a contagem de 1 até 100, usando apenas de
 # números ímpares, ao final do processo exiba na tela quantos números ímpares
 # foram encontrados nesse intervalo, assim como a soma dos mesmos.
 import os
 
 
+class Numeros:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def exibir(self, c_impar, somatorio):
+        print(f'\nExiste {c_impar} número(s) ímpar(es) nesse intervalo. '
+              + f'A soma de todos os valores é igual a: {somatorio}')
+
+
+class Impares(Numeros):
+    def impar(self):
+        try:
+            c_impar = 0
+            somatorio = 0
+            for c in range(int(self.inicio), (int(self.fim) + 1)):
+                if c % 2 != 0:
+                    print(c, end=' | ')
+                    c_impar += 1
+                    somatorio += c
+            self.exibir(c_impar, somatorio)
+        except (TypeError, ValueError):
+            print('Insira apenas valores númericos e inteiros! ')
+
+
 os.system('cls')
 
 print('NÚMEROS ÍMPARES EM UM INTERVALO E SUA QUANTIDADE E SOMA TOTAL')
-inicio = int(input('Insira um valor inteiro para iniciar o intervalo..: '))
-final = int(input('Insira um valor inteiro para finalizar o intervalo: '))
+inicio = input('Insira um valor inteiro para iniciar o intervalo..: ')
+final = input('Insira um valor inteiro para finalizar o intervalo: ')
 print()
-c_impar = 0
-somatorio = 0
-
-for c in range(inicio, (final +1)):
-    
-    if (c % 2 != 0) == True:
-        print(c, end=' | ')
-        c_impar += 1
-        somatorio += c
-                
-print('\n')       
-print(f'Existe {c_impar} número(s) ímpar(es) nesse intervalo.')
-print()
-print(f'A soma de todos os valores é igual a: {somatorio}')
+impar = Impares(inicio, final)
+impar.impar()
