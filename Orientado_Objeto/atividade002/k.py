3f5c53d3b760810b0e70ada79b11dc0599d2d792
DianLuca <dianlucca@hotmail.com>
2024-09-09 14:39:41 -0300

    Desenvolvimento da atividade k.py

--- Orientado_Objeto/atividade002/k.py
+++ Orientado_Objeto/atividade002/k.py
@@ -2,22 +2,34 @@
 # Turma 0152
 # Autor: Dian Luca Valente Nascimento
 # Data: 03/09/2024
-# Crie um programa que pede que o usuário digite um nome ou uma frase, 
+# Crie um programa que pede que o usuário digite um nome ou uma frase,
 # verifique se esse conteúdo digitado é um palíndromo ou não, exibindo
 # em tela esse resultado.
 import os
 
 
+class Palavra():
+    def __init__(self, entrada):
+        self.palavra = entrada
+
+    def exibir(self, mensagem):
+        print(mensagem)
+
+
+class Palindromo(Palavra):
+    def verificar(self):
+        palindromo = self.palavra[::-1]
+        if self.palavra == palindromo:
+            self.exibir(f'A palavra {self.palavra} é um palíndromo, pois {self.palavra} ao contrário: "{palindromo}" ficam iguais!')
+        else:
+            self.exibir(f'A palavra {self.palavra} não é um palíndromo, pois {self.palavra} ao contrário: "{palindromo}" não são iguais!')
+
+
 os.system('cls')
 
-print('-- VERFICAÇÃO DE PALÍNDROMO --') # Quando um nome ou uma frase é 
-                                        # igual mesmo ao contrário
-nome = input('Insira seu nome ou uma frase: ')
-palindromo = nome[::-1]
+print('-- VERIFICAÇÃO DE PALÍNDROMO --')  # Quando um nome ou uma frase é
+# igual mesmo ao contrário
+nome = input('Insira seu nome ou uma frase: ').strip().lower()
+palindromo = Palindromo(nome)
+palindromo.verificar()
 
-if nome == palindromo:
-    print(f'Trata-se de um palíndromo, pois {nome} e {palindromo} ' 
-          + f'são iguais.')
-else:
-    print(f'Não se trata de um palíndromo pois {nome} e {palindromo} '
-          + f'formam palavras diferentes')
